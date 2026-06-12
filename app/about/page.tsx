304fc4c0d03b8076ea146830dfbbc0bf61800f3d
Ebram-Barsoum <abram.barsoum3.89@gmail.com>
2026-06-13 01:01:22 +0300

    feat: add description to metadata object

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,6 +9,13 @@ import AnimationWrapper from '@/shared/animation/AnimationWrapper';
 
 export const metadata: Metadata = {
     title: "About",
+    description: `
+    I'm Ebram Barsoum, I'm a React & Next.js Software Engineer with 2 years of experience building scalable, production-grade web
+                    applications including multi-vendor e-commerce platforms, trading systems, and real-time dashboards.
+                    Strong expertise in modern Next.js features (Routing, Server Actions, Middleware, SSR/SSG/ISR) and
+                    TypeScript. Passionate about clean architecture, performance optimization, and building maintainable
+                    systems.
+    `
 };
 
 export default function AboutPage(): JSX.Element {
